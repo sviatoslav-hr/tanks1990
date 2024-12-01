a7b509f4a02110e26f8fb08b3adcacd20a1117b8
Sviatoslav <shot.ngh@gmail.com>
2024-12-01 16:04:36 +0200

    fix: fix window global setting

--- src/globals.ts
+++ src/globals.ts
@@ -1,6 +1,9 @@
 import {assert, assertError, throwError} from '#/utils';
 
-(globalThis as any).window = globalThis;
+if (!globalThis.window) {
+    // HACK: make `window` global available in tests
+    (globalThis as any).window = globalThis;
+}
 window.assert = assert;
 window.assertError = assertError;
 window.panic = throwError;
