cdc687a6bdaf6b36cbec21596d0b900e6aac0c27
Sviatoslav <shot.ngh@gmail.com>
2025-01-18 14:17:38 +0200

    refactor: move game loop into main file

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import './globals';
 import './style.css';
 
+import {Color} from '#/color';
 import {APP_ELEMENT_ID, BASE_HEIGHT, BASE_WIDTH, DEV_MODE_KEY} from '#/const';
-import {createDevUI} from '#/dev-ui';
+import {createDevUI, DevUI} from '#/dev-ui';
 import {preloadEffectImages} from '#/entity/effect';
-import {runGame} from '#/game';
 import {GameInput} from '#/game-input';
+import {handleGameInputTick} from '#/game-input-handler';
 import {Rect} from '#/math';
-import {initMenu} from '#/menu';
+import {Duration} from '#/math/duration';
+import {initMenu, Menu} from '#/menu';
 import {Renderer} from '#/renderer';
+import {drawScore, saveBestScore} from '#/score';
 import {SoundManager} from '#/sound';
 import {GameState} from '#/state';
 import {GameStorage} from '#/storage';
 import {World} from '#/world';
+
+main();
 
 function main(): void {
     const appElement = document.getElementById(APP_ELEMENT_ID);
@@ -47,4 +52,81 @@ function main(): void {
     runGame(gameState, world, menu, input, storage, devUI, renderer);
 }
 
-main();
+function runGame(
+    state: GameState,
+    world: World,
+    menu: Menu,
+    input: GameInput,
+    storage: GameStorage,
+    devUI: DevUI,
+    renderer: Renderer,
+) {
+    renderer.resizeCanvas(window.innerWidth, window.innerHeight);
+    menu.resize(renderer.canvas.clientWidth, renderer.canvas.clientHeight);
+    menu.showMain();
+    input.listen(document.body, renderer.canvas);
+
+    window.addEventListener('resize', () => {
+        renderer.resizeCanvas(window.innerWidth, window.innerHeight);
+        menu.resize(renderer.canvas.clientWidth, renderer.canvas.clientHeight);
+    });
+
+    let lastTimestamp = 0;
+    const animationCallback = (timestamp: number): void => {
+        const dt = Duration.since(lastTimestamp).min(1000 / 30);
+        lastTimestamp = timestamp;
+
+        try {
+            handleGameTick(dt, state, world, menu, input, storage, renderer);
+            handleGameInputTick(
+                input,
+                renderer,
+                state,
+                world,
+                menu,
+                devUI,
+                storage,
+            );
+            devUI.update(dt);
+            input.nextTick();
+            state.nextTick();
+        } catch (err) {
+            console.error('Error in animationCallback', err);
+        }
+
+        window.requestAnimationFrame(animationCallback);
+    };
+
+    window.requestAnimationFrame(animationCallback);
+}
+
+function handleGameTick(
+    dt: Duration,
+    state: GameState,
+    world: World,
+    menu: Menu,
+    input: GameInput,
+    storage: GameStorage,
+    renderer: Renderer,
+) {
+    renderer.setFillColor(Color.BLACK_RAISIN);
+    renderer.fillScreen();
+
+    world.draw(renderer);
+
+    if (state.paused || state.dead || (state.playing && input.isDown('KeyQ'))) {
+        drawScore(renderer, world.player, storage);
+    }
+
+    if (world.player.dead && state.playing && !menu.dead) {
+        menu.showDead();
+        saveBestScore(storage, world.player.score);
+    }
+
+    if (state.playing || state.debugUpdateTriggered) {
+        world.update(dt, renderer.camera);
+        if (world.isInfinite) {
+            renderer.camera.centerOn(world.player);
+        }
+    }
+}
